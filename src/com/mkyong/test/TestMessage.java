ebe8bf333d36e8ed02bc7380c9cf53c1dd20fa95
Shaowei Wang <sw2396@engr.msstate.edu>
2021-02-18 11:00:20 -0600

    another case

---
+++ src/com/mkyong/test/TestMessage.java
@@ -0,0 +1,20 @@
+package com.mkyong.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.mkyong.message.MessageGenerator;
+
+public class TestMessage {
+
+	@Test
+	public void test_welcome_message() {
+
+		MessageGenerator obj = new MessageGenerator();
+		// correct is welcome
+		assertEquals("welcome", obj.getWelcomeMessage());
+
+	}
+ 
+}
